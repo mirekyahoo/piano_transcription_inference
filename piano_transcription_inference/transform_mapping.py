25734a58598dea180f8d6652644744a381f3bf9c
mirekyahoo <narbutt@yahoo.com>
2026-04-21 09:02:51 +0100

    Add files via upload

---
+++ piano_transcription_inference/transform_mapping.py
@@ -0,0 +1,19 @@
+
+#def transform25(x: float) -> float:
+#    x = float(x)
+#    y = x*127;
+#    return y
+
+# (...)
+
+def transform69(x: float) -> float:
+    x = float(x)
+    y = x*127;
+    return y
+
+# (...)
+
+#def transform104(x: float) -> float:
+#    x = float(x)
+#    y = x*127;
+#    return y
